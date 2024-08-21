b653b43e17e836cdf38088f4bb28c9b1e67a3c42
0y3 <trivin98@gmail.com>
2024-08-21 12:48:55 +0100

    spring initializr

---
+++ src/main/java/com/testrest/restapi/RestapiApplication.java
@@ -0,0 +1,13 @@
+package com.testrest.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiApplication.class, args);
+	}
+
+}
